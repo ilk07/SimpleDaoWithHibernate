321c0cf355afaff1ed0208a1ad9f87ff22a6bd65
ilk07 <ilk07@inbox.ru>
2023-05-11 19:24:54 +0300

    update RequestParam annotations

--- src/main/java/com/homeworks/simpledaolayerhibernatejpa/controller/PersonsController.java
+++ src/main/java/com/homeworks/simpledaolayerhibernatejpa/controller/PersonsController.java
@@ -2,6 +2,7 @@ package com.homeworks.simpledaolayerhibernatejpa.controller;
 
 import com.homeworks.simpledaolayerhibernatejpa.entity.Person;
 import com.homeworks.simpledaolayerhibernatejpa.repository.PersonsRepository;
+import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +15,28 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/persons")
 public class PersonsController {
+
     @Autowired
     PersonsRepository personsRepository;
 
+    @GetMapping()
+    public String getInfo() {
+        return "Hi! To get data by city, name or age, please, login";
+    }
+
     @GetMapping("/by-city")
-    public List<Person> getPersonByCity(@RequestParam(required = true) String city) {
+    public List<Person> getPersonByCity(@RequestParam(required = false) String city) {
         return personsRepository.findByCity(city);
     }
 
+
     @GetMapping("/by-age")
-    public List<Person> getPersonByAgeLess(@RequestParam(required = true) int age) {
+    public List<Person> getPersonByAgeLess(@RequestParam(required = false) int age) {
         return personsRepository.findByAgeLessThanOrderByAgeAsc(age);
     }
 
     @GetMapping("/by-name-surname")
-    public Optional<Person> getPersonByNameSurname(@RequestParam(required = true) String name, String surname) {
+    public Optional<Person> getPersonByNameSurname(@RequestParam(required = false) String name, String surname) {
         return personsRepository.findByNameAndSurname(name, surname);
     }
 
