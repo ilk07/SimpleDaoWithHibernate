ca1e28aa1d3dc7898f51ed7240e0ca0758498611
ilk07 <ilk07@inbox.ru>
2023-05-13 20:38:15 +0300

    add secure annotations, ad method checkUsername

--- src/main/java/com/homeworks/simpledaolayerhibernatejpa/controller/PersonsController.java
+++ src/main/java/com/homeworks/simpledaolayerhibernatejpa/controller/PersonsController.java
@@ -1,13 +1,26 @@
+/*
+Напишите новый контроллер, в котором:
+- один из методов возвращает значения только для пользователей с ролью "READ" (используйте @Secured);
+- один из методов возвращает значения только для пользователей с ролью "WRITE" (используйте @RolesAllowed);
+- один из методов возвращает значения, если у пользователя есть хотя бы одна из ролей из "WRITE", "DELETE" (используйте pre/post аннотации);
+- один из методов, который принимает в качестве query-параметра имя пользователя (username),
+должен возвращает значения, только если у пользователя username совпадает с именем пользователя в вашем объекте
+Authentication, который Spring security сохраняет в SecurityContextHolder после успешной аутентификации.*
+*/
 package com.homeworks.simpledaolayerhibernatejpa.controller;
 
 import com.homeworks.simpledaolayerhibernatejpa.entity.Person;
 import com.homeworks.simpledaolayerhibernatejpa.repository.PersonsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,20 +36,31 @@ public class PersonsController {
         return "Hi! To get data by city, name or age, please, login";
     }
 
+    @Secured({"ROLE_READ"})
     @GetMapping("/by-city")
-    public List<Person> getPersonByCity(@RequestParam(required = false) String city) {
-        return personsRepository.findByCity(city);
+    public Optional<List<Person>> getPersonByCity(@RequestParam(required = false) String city) {
+        return Optional.ofNullable(personsRepository.findByCity(city));
     }
 
-
+    @RolesAllowed({"ROLE_WRITE"})
     @GetMapping("/by-age")
-    public List<Person> getPersonByAgeLess(@RequestParam(required = false) int age) {
-        return personsRepository.findByAgeLessThanOrderByAgeAsc(age);
+    public Optional<List<Person>> getPersonByAgeLess(@RequestParam(required = true) int age) {
+        if (age >= 0) {
+            return Optional.ofNullable(personsRepository.findByAgeLessThanOrderByAgeAsc(age));
+        }
+        return null;
     }
 
+    @PreAuthorize("hasAnyRole('READ','DELETE')")
     @GetMapping("/by-name-surname")
     public Optional<Person> getPersonByNameSurname(@RequestParam(required = false) String name, String surname) {
         return personsRepository.findByNameAndSurname(name, surname);
+    }
+
+    @PostAuthorize("#username.equalsIgnoreCase(authentication.principal.username)")
+    @GetMapping("/by-username")
+    public String checkUsername(@RequestParam String username) {
+        return "Ok";
     }
 
 }
